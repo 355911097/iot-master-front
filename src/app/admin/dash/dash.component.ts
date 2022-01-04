6c0b5dbfa655a66de13c04f7bf274a94c16482af
Jason <jason@zgwit.com>
2022-01-04 19:55:41 +0800

    不再使用多标签页，自己封装的有问题，nz-tabs不能动态内容

--- src/app/admin/dash/dash.component.ts
+++ src/app/admin/dash/dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminComponent} from "../admin.component";
 
 @Component({
   selector: 'app-dash',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashComponent implements OnInit {
 
-  constructor() {
+  constructor(admin: AdminComponent) {
 
   }
 
